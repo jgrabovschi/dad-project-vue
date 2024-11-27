614a944c7f05904c954edf7119602f2a9a55f088
jgrabovschi <iorgusor@gmail.com>
2024-11-27 20:29:46 +0000

    Add pagination to Game History component and enhance game status display

--- src/stores/games.js
+++ src/stores/games.js
@@ -1,27 +1,47 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import { useAuthStore } from '@/stores/auth'
 import { useErrorStore } from '@/stores/error'
 import axios from 'axios'
 
 export const useGamesStore = defineStore('games', () => {
     const storeError = useErrorStore()
-    const storeAuth = useAuthStore()
     const games = ref([])
-
-
-    const clearGames = () => {
-        games.value = []
-    }
+    const pages = ref(0)
+    const currentPage = ref(1)
+    const totalItems = ref(0)
 
     const loadGames = async () => {
         storeError.resetMessages()
         try {
-            const response = await axios.get('/games')
+            const response = await axios.get('/games?page=' + currentPage.value)
             games.value = response.data.data
+            pages.value = response.data.meta.last_page
+            currentPage.value = response.data.meta.current_page
+            totalItems.value = response.data.meta.total
+
         } catch (e) {
             storeError.setErrorMessages(e.response.data.message, e.response.data.errors, e.response.status, 'Getting Games Error!')
         }
     }
-  return { clearGames, loadGames, games }
+
+    const nextPage = () => {
+        if (currentPage.value < pages.value) {
+            currentPage.value++
+            loadGames()
+        }
+    }
+
+    const previousPage = () => {
+        if (currentPage.value > 1) {
+            currentPage.value--
+            loadGames()
+        }
+    }
+
+    const toPage = (page) => {
+        currentPage.value = page
+        loadGames()
+    }
+
+  return { loadGames, games, pages, currentPage, totalItems, nextPage, previousPage, toPage}
 })
