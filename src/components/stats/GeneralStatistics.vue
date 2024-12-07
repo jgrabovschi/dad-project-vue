cbaa237c29809bbdc8cf1bc6b2dbde35414a3898
jgrabovschi <iorgusor@gmail.com>
2024-12-07 14:45:41 +0000

    Add Statistics component with loading state and general statistics display

---
+++ src/components/stats/GeneralStatistics.vue
@@ -0,0 +1,38 @@
+<script setup>
+import axios from 'axios';
+import { VueSpinnerPacman } from 'vue3-spinners';
+import { ref } from 'vue';
+
+const stats = ref(null)
+const isLoading = ref(false)
+
+
+</script>
+
+<template>
+    <div v-if="isLoading" class="flex justify-center items-center h-32">
+        <VueSpinnerPacman size="30" color="gray" />
+    </div>
+    <div v-else class="space-y-2 m-4">
+        <div class="flex justify-between items-center">
+            <span class="font-semibold dark:text-slate-100">Total Games Registered (all users):</span>
+            <span class="dark:text-slate-300">123</span>
+        </div>
+        <div class="flex justify-between items-center">
+            <span class="font-semibold dark:text-slate-100">Total Singleplayer Games:</span>
+            <span class="dark:text-slate-300">123</span>
+        </div>
+        <div class="flex justify-between items-center">
+            <span class="font-semibold dark:text-slate-100">Total Multiplayer Games:</span>
+            <span class="dark:text-slate-300">123</span>
+        </div>
+        <div class="flex justify-between items-center">
+            <span class="font-semibold dark:text-slate-100">Total Users:</span>
+            <span class="dark:text-slate-300">123</span>
+        </div>
+        <div class="flex justify-between items-center">
+            <span class="font-semibold dark:text-slate-100">Total Games Played:</span>
+            <span class="dark:text-slate-300">123</span>
+        </div>
+    </div>
+</template>
--- src/components/stats/Statistics.vue
+++ src/components/stats/Statistics.vue
@@ -1,5 +1,41 @@
 <script setup>
+import { Card } from '@/components/ui/card'
+import { CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { ref } from 'vue'
+import GeneralStatistics from './GeneralStatistics.vue';
+
+const stats = ref(null)
+const isLoading = ref(false)
+
 </script>
 <template>
-
+    <div class="flex justify-center items-center p-4">
+        <Card class="w-full max-w-6xl h-auto rounded-lg bg-white dark:bg-gray-800 border-0 shadow-md">
+            <CardHeader class="px-4">
+                <CardTitle class="text-lg md:text-xl text-black dark:text-white">Statistics</CardTitle>
+                <CardDescription class="text-sm md:text-base">Here you can see the overall statistics about this game.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div class="overflow-x-auto">
+                    <Tabs default-value="general" class="w-[400px]">
+                        <TabsList class="dark:bg-slate-600 dark:text-slate-300">
+                            <TabsTrigger value="general">
+                                General
+                            </TabsTrigger>
+                            <TabsTrigger value="password">
+                                Password
+                            </TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="general">
+                            <GeneralStatistics />
+                        </TabsContent>
+                        <TabsContent value="password">
+                            Change your password here.
+                        </TabsContent>
+                    </Tabs>
+                </div>
+            </CardContent>
+        </Card>
+    </div>    
 </template>
